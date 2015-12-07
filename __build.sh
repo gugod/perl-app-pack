abf5e051db01bb22f57f06df55e6e3426cc6e515
Kang-min Liu <gugod@gugod.org>
2015-12-07 22:18:48 +0100

    Build with fatpack.

--- __build.sh
+++ __build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+set -x
+
+function __fatpack_one_executable() {
+    filename=$1
+    TMPDIR=`mktemp -d /tmp/perl-app-pack.XXXXXX` || exit 1
+    cd $TMPDIR
+    fatpack trace $filename
+    fatpack packlists-for `cat fatpacker.trace` > packlists
+    fatpack tree `cat packlists`
+    (echo '#!/usr/bin/env perl'; fatpack file; cat $filename)
+    rm -rf $TMPDIR
+    cd -
+}
 
 perlenv=perl-5.22.0@perl-app-pack
 
 cd $(dirname $0)
+rm -rf fatlib;
 
 eval "$(perlbrew init-in-bash)"
 perlbrew use ${perlenv}
@@ -14,31 +28,25 @@ else
     exit 1
 fi
 
-git clean -d -f
-git pull
+MODULES="CPAN::Uploader App::depak App::FatPacker::Simple Perl::Strip common::sense App::FatPacker Minilla Dist::Zilla"
+cpanm -L local $MODULES || exit 1
 
-cpanm common::sense App::FatPacker Minilla || exit 1
+PATH=`pwd`/local/bin:$PATH
+PERL5LIB=`pwd`/local/lib/perl5:$PERL5LIB
 hash -r
 
-fatpack trace `which minil`
-fatpack packlists-for `cat fatpacker.trace` >packlists
-fatpack tree `cat packlists`
-(echo "#!/usr/bin/env perl"; fatpack file; cat `which minil`) > minil
-chmod +x minil
+__fatpack_one_executable `pwd`/local/bin/minicpan > minicpan
+__fatpack_one_executable `pwd`/local/bin/dzil     > dzil
 
-exit;
+git add minil dzil
 
-versions=$(perl -MApp::FatPacker -MMinilla -e 'print "minilla: " . Minilla->VERSION . " facpacker:" . App::FatPacker->VERSION . "\n"')
-
-git add minil
-
-git_changed=$(git status --porcelain minil | grep minil)
+git_changed=$(git status --porcelain)
 
 if [[ "$git_changed" == "" ]]; then
     echo "Nothing changed. Skip committing."
     exit 0
 else
-    git commit -m "rebuild with $versions"
+    git commit -m "rebuild"
     git push
     git clean -d -f
     git pull
