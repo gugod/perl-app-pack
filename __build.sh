055446235ff37328a0068ee18987634184c1d011
Kang-min Liu <gugod@gugod.org>
2015-12-08 00:19:41 +0100

    Add xkcdpass

--- __build.sh
+++ __build.sh
@@ -27,14 +27,14 @@ else
     exit 1
 fi
 
-MODULES="App::hr App::pmuninstall App::Perldoc::Search App::PMUtils App::jt App::es App::YG App::cpm App::pmdir App::tchart"
+MODULES="App::hr App::pmuninstall App::Perldoc::Search App::PMUtils App::jt App::es App::YG App::cpm App::pmdir App::tchart App::xkcdpass"
 cpanm -L local $MODULES
 
 PATH=`pwd`/local/bin:$PATH
 PERL5LIB=`pwd`/local/lib/perl5:$PERL5LIB
 hash -r
 
-for executable in hr pm-uninstall perldoc-search pmlist jt es yg cpm pmdir tchart
+for executable in hr pm-uninstall perldoc-search pmlist jt es yg cpm pmdir tchart xkcdpass
 do
     ex=`pwd`/local/bin/${executable}
     if [[ -f $ex ]]; then
