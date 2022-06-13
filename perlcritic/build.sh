1b91d40a61ed021b92a1e220b9a1d59bf760a9db
Kang-min Liu <gugod@gugod.org>
2022-06-13 23:23:52 +0900

    always add :latest tag

--- perlcritic/build.sh
+++ perlcritic/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build . --tag docker.io/gugod/perlcritic:$(date +%Y%m%d)
+docker build . --tag docker.io/gugod/perlcritic:$(date +%Y%m%d) --tag docker.io/gugod/perlcritic:latest
